5a9523cc912121bbfe2cca85c2f8a3e14bbc4120
baxia.lan <baxia.lan@gmail.com>
2017-04-13 15:27:37 -0400

    train a perceptron model based on tweet text and its sentiment

--- classification/twitter2kafka_producer.py
+++ classification/twitter2kafka_producer.py
@@ -3,7 +3,10 @@
 """
 @author: Jillian
 
-Upload filtered tweets got from Twitter API to Kafka by KafkaProducer()
+Upload filtered tweets from Twitter API to Kafka by KafkaProducer()
+classify based on text and sentiment(as label)
+train a perceptron model
+Send Notification if predict label is 1.0
 
 """
 #Import the necessary methods from tweepy library
@@ -15,7 +18,11 @@ import json
 import string
 import random
 import time
-#import boto3
+import tweet_utils
+import boto3
+from perceptron import PerceptronforRDD
+from pyspark.mllib.feature import HashingTF, IDF
+import pyspark
 
 # change default system encoding method of python into UTF-8
 import sys
@@ -27,7 +34,9 @@ from kafka.errors import KafkaError
 #producer = KafkaProducer(bootstrap_servers=['localhost:9092'])#, value_serializer=lambda v: json.dumps(v).encode('ascii'))
 
 
-
+sc=pyspark.SparkContext(appName="twittertest")
+sc.setLogLevel("WARN")
+model = PerceptronforRDD(200)
 
 #Variables that contains the user credentials to access Twitter API
 access_token = "***REMOVED***"
@@ -40,17 +49,19 @@ consumer_secret = "***REMOVED***"
 class TwitterListener(StreamListener):
     def __init__(self):
         self.producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+        self.client_sns = boto3.client('sns')
+        self.sns_arn = 'arn:aws:sns:us-west-2:560376101737:tweets-feed'
 
     def on_data(self, data):
         try:
             tweet = json.loads(data) # load tweet content into json format
 
-            if "place" in tweet.keys() and tweet["place"]!=None:# and tweet["lang"]=="en":
+            if "place" in tweet.keys() and tweet["place"]!=None and tweet["lang"]=="en":
                 # process coordinates information, format: [longitude, latitude]
                 Box = tweet["place"]["bounding_box"]["coordinates"][0]
                 XY = [(Box[0][0] + Box[2][0])/2, (Box[0][1] + Box[2][1])/2]
                 # extract information into defined body format
-                print tweet["text"]
+                #print tweet["text"]
                 doc = {
                        "timestamp": tweet["timestamp_ms"],
                        "username": tweet["user"]["name"],
@@ -60,12 +71,34 @@ class TwitterListener(StreamListener):
                        #"hashtags": tweet["entities"]["hashtags"],
                        "tweet_text": tweet["text"]
                        }
+                tweet_utils._sentiment_analysis(doc)
+                data = {"text":doc["tweet_text"], "label":doc["sentiment"]}
+                if data["label"]=='Negative' or data["label"]=='Neutral':
+                    data["label"]=-1.0
+                elif data["label"]=='Positive':
+                    data["label"]=1.0
+                print 'Training data:\n',data
+                #print '\n'
+                [tf, labels] = Tf([data])
+                print 'Training data after TF:\n',[tf.first(),labels.first()]
+                #print '\n'
+                [model,w,b] = ClassifybyPerceptron(tf,labels)
+                print 'Perceptron weight:\n',[w,b]
+                #print '\n'
+                predict_label = model.Predict(tf)
+                print 'Predict sentiment:\n',predict_label.first()
+                #print '\n'
+                #errrate = model.PredictErrrate(tf,labels)
+                #print 'Training error rate:', errrate
                 json_body = json.dumps(doc)
                 print json_body
-
                 # Send messages to Kafka Topic 'twitterstream'
-                self.producer.send('twitterstream', json_body)
-                print 'Write To Kafka Complete' + '\n'
+                if predict_label.first()==1.0:
+                    self.producer.send('twitterstream', json_body)
+                    print 'Write To Kafka Complete' + '\n'
+                    self.client_sns.publish(TopicArn=self.sns_arn, Message=json_body, Subject='Tweet')
+                    print 'message pushed'
+                print '\n\n\n'
 
 
         except Exception, e:
@@ -92,7 +125,38 @@ class TwitterListener(StreamListener):
     def on_connect(self):
         print (self)
 
+def Tfidf(data):
+	training_raw = sc.parallelize(data)
+	labels = training_raw.map(
+    	lambda doc: doc["label"],  # Standard Python dict access
+    	preservesPartitioning=True # This is obsolete.
+	)
+	# While applying HashingTF only needs a single pass to the data, applying IDF needs two passes:
+	# First to compute the IDF vector and second to scale the term frequencies by IDF.
+	tf = HashingTF(numFeatures=200).transform( ## Use much larger number in practice
+    	training_raw.map(lambda doc: doc["text"].split(),
+    	preservesPartitioning=True))
+	#tf.cache()
+	idf = IDF().fit(tf)
+	tfidf = idf.transform(tf)
+	return tfidf
 
+def Tf(data):
+	training_raw = sc.parallelize(data)
+	labels = training_raw.map(
+    	lambda doc: doc["label"],  # Standard Python dict access
+    	preservesPartitioning=True # This is obsolete.
+	)
+	# While applying HashingTF only needs a single pass to the data, applying IDF needs two passes:
+	# First to compute the IDF vector and second to scale the term frequencies by IDF.
+	tf = HashingTF(numFeatures=200).transform( ## Use much larger number in practice
+    	training_raw.map(lambda doc: doc["text"].split(),
+    	preservesPartitioning=True))
+	return [tf,labels]
+
+def ClassifybyPerceptron(tf,label):
+	[w,b] = model.AveragePerceptron(tf, label)
+	return [model,w,b]
 
 def main():
     #This handles Twitter authetification and the connection to Twitter Streaming API
@@ -107,7 +171,7 @@ def main():
     while True:
         try:
             #This line filter Twitter Streams to capture data by the keywords
-            stream.filter(track=terms,languages=["en"])## This will feed the stream all mentions of 'keyword'
+            stream.filter(track=terms,filter_level='low',languages=["en"])## This will feed the stream all mentions of 'keyword'
             #stream.sample()##This will feed the stream without keywords or filter
             break
         except Exception, e:
