7547d155a3082e9b2296e3687c590693ca590d1b
Haoyan Min <hm2689@columbia.edu>
2017-04-23 20:40:06 -0700

    completed Classifier_B

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 import requests,json
 import push
+import ES
 
 application = Flask(__name__)
 application.config["DEBUG"] = True
-
+#file_t=open('reviews_tr1000.txt','r')
+#file_l=open('reviews_tr1000_label.txt','r')
 @application.route("/",methods=["POST","GET"])
 def hello():
 	text=['ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?']
@@ -19,15 +21,20 @@ def feedback():
 	print y
 	tuple=[]
 	for i in range(len(y)):
-		tuple.append({'text':z[i],'label':y[i]})
+		if z[i]!='Tooooo fast!!' and z[i]!='ready?':
+			tuple.append({'text':z[i],'label':y[i]})
 	print tuple
 
-	if tuple[0]['text']!='0':
+	if len(tuple)!=0:
 		push.push(json.dumps(tuple))
 
-	text=['Second','Masterpiece','of','Princess','Lan','and','Her','Photographer','Mina','Lisa']
-
-	myvar={"text":text}
+	text=ES.pull(10)
+#	label=''
+#	for i in range(10):
+#		text.append(file_t.readline())
+#		label=label+file_l.readline()
+#	myvar={"text":text,"label":label}
+	myvar={'text':text}
 	jvar=json.dumps(myvar)
 	return jvar
 
