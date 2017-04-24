04cf3bfcba732be849673143caaf1d5c193da02b
Haoyan Min <hm2689@columbia.edu>
2017-04-24 14:32:54 -0700

    Add files via upload

---
+++ Pull_from_ES.py
@@ -0,0 +1,39 @@
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+import json
+
+# change default system encoding method of python into UTF-8
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
+#Variables that contains the user credentials to access AWS Elastucsearch
+host = '***REMOVED***'
+awsauth = AWS4Auth('***REMOVED***', '***REMOVED***', 'us-east-1', 'es')
+
+es = Elasticsearch(
+    hosts=[{'host': host, 'port': 443}],
+    http_auth=awsauth,
+    use_ssl=True,
+    verify_certs=True,
+    connection_class=RequestsHttpConnection
+)
+
+def pull(category,max_num=10):
+	L=[]
+	like=category+'_like'
+	dislike=category+'_dislike'
+	response = es.search(index='tweets',doc_type=like,size=max_num)
+	print response['hits']['hits']
+	for element in response['hits']['hits']:
+		print element['_source']
+		L.append(element['_source']['tweet_text'])
+		es.delete('tweets',like,element['_id'])
+	if len(L)<10:
+		response_2 = es.search(index='tweets',doc_type=dislike,size=max_num-len(L))
+		for element in response_2['hits']['hits']:
+			L.append(element['_source']['tweet_text'])
+			es.delete('tweets',dislike,element['_id'])
+	print L
+	return L
--- application.py
+++ application.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 import requests,json
 import push
-import ES
+import Pull_from_ES
 
 application = Flask(__name__)
 application.config["DEBUG"] = True
-#file_t=open('reviews_tr1000.txt','r')
-#file_l=open('reviews_tr1000_label.txt','r')
+
 @application.route("/",methods=["POST","GET"])
 def hello():
 	text=['ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?','ready?']
@@ -16,6 +15,8 @@ def hello():
 def feedback():
 	f=request.args.getlist('f[]')
 	t=request.args.getlist('text[]')
+	category=request.args.get('category')
+	print 'category',category
 	y=[int(i) for i in f]
 	z=[str(i) for i in t]
 	print y
@@ -28,12 +29,8 @@ def feedback():
 	if len(tuple)!=0:
 		push.push(json.dumps(tuple))
 
-	text=ES.pull(10)
-#	label=''
-#	for i in range(10):
-#		text.append(file_t.readline())
-#		label=label+file_l.readline()
-#	myvar={"text":text,"label":label}
+	text=Pull_from_ES.pull(str(category))
+	#text=['x','y']
 	myvar={'text':text}
 	jvar=json.dumps(myvar)
 	return jvar
