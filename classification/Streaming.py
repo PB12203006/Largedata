481b675373c7d3970f730e00ae80250b86110357
Haoyan Min <hm2689@columbia.edu>
2017-04-25 13:21:25 -0700

    Add files via upload

--- classification/Streaming.py
+++ classification/Streaming.py
@@ -9,7 +9,7 @@ from perceptron import PerceptronforRDD
 import json
 
 import Push_to_ES
-import tweets_category_predict_nb
+from perceptronMulticlass import MulticlassPerceptron
 #initialize
 sc=pyspark.SparkContext(appName="akftest")
 sc.setLogLevel("WARN")
@@ -19,6 +19,15 @@ topic_2=["twitterstream_raw"]
 topic_3=["feedback_A"]
 model = PerceptronforRDD(2000)
 spark = SparkSession(sc)
+categ = ['Art & Design','World','Sports','Fashion & Style','Books','Music', \
+            'Television','Movies','Technology','Science','Food','Real Estate','Theater', \
+            'Health','Travel','Education','Your Money','Politics','Economy']
+dictionary = {'Art & Design':0,'World':1,'Sports':2,'Fashion & Style':3,'Books':4,'Music':5, \
+            'Television':6,'Movies':7,'Technology':8,'Science':9,'Food':10,'Real Estate':11,'Theater':12, \
+            'Health':13,'Travel':14,'Education':15,'Your Money':16,'Politics':17,'Economy':18}
+
+multiperceptron = MulticlassPerceptron(numFeatures=2000,numClasses=19,dictionary=dictionary,category=categ)
+perceptronmodels = multiperceptron.load("/home/pb12203006/Documents/Largedata_with_Princess/classification/perceptronModels.json")
 
 '''
 init_rdd=sc.emptyRDD()
@@ -85,6 +94,22 @@ def PreferencePerceptron(tf,label):
 	[w,b] = model.AveragePerceptron(tf, label)
 	return [model,w,b]
 
+def process_feedback_A(rdd):
+	if rdd.count()!=0:
+		rdd = rdd.map(lambda x: x[-1])
+		print rdd.collect()
+		for element in rdd.collect():
+			data = json.loads(element)
+			tfml = Tfml_feedback_A(data)
+			category = sc.parallelize(data).map(lambda x:x["category"])
+			print "count perceptron classifier:", multiperceptron.models[0].count_avg
+			models =multiperceptron.train(tfml,category)
+			print "actrual category:", data[0]["category"]
+			print "count perceptron classifier after training:", models[0].count_avg
+			print "predict tweet category:", multiperceptron.predict(tfml)
+			print "\n"
+
+			
 
 #if rdd nonempty, do something
 def process_feedback_B(rdd):
@@ -106,6 +131,18 @@ def process_feedback_B(rdd):
 			print('Training error rate:', errrate)
 			print '\n\n\n\n\n\n'
 			#call any function you like
+
+def Tfml_feedback_A(data):
+	raw_data = sc.parallelize(data)
+	lines = raw_data.map(lambda doc: doc["text"]).map(lambda line: Row(sentence=line))
+	sentenceData = lines.toDF()
+	#sentenceData.show()
+	tokenizer = Tokenizer(inputCol="sentence", outputCol="words")
+	wordsData = tokenizer.transform(sentenceData)
+	hashingTF = HashingTF(inputCol="words", outputCol="features", numFeatures=2000)
+	featurizedData = hashingTF.transform(wordsData)
+	tf = featurizedData.select('features').rdd.map(lambda x:x.features)
+	return tf
 
 def Tfml_tweets(data):
 	raw_data = sc.parallelize(data)
@@ -131,9 +168,11 @@ def process_tweets(rdd):
 		#	print tfml.collect()
 			labels=model.Predict(tfml)
 			print labels.collect()[0]
-			categories = tweets_category_predict_nb.predictTweetCategNB(tfml.collect(),sc)
+			#categories = tweets_category_predict_nb.predictTweetCategNB(tfml.collect(),sc)
+			#print categories
+			categories = multiperceptron.predict(tfml)
+			#print categories
 			print categories
-			#category = 'R'
 			for category in categories:
 				if labels.collect()[0]>0:
 					category=category+'_like'
@@ -154,7 +193,7 @@ Stream_feedback_B.pprint()
 Stream_feedback_A = KafkaUtils.createDirectStream(ssc,topic_3,kafkaparams)
 Stream_feedback_A.pprint()
 Stream_rawtweets = KafkaUtils.createDirectStream(ssc,topic_2,kafkaparams)
-#operate rdd
+Stream_feedback_A.foreachRDD(lambda k: process_feedback_A(k))
 Stream_feedback_B.foreachRDD(lambda k: process_feedback_B(k))
 Stream_rawtweets.pprint()
 Stream_rawtweets.foreachRDD(lambda k: process_tweets(k))
