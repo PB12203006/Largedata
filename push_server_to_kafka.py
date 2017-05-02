ba520148432cf086dc68435ba0537c83dcf45185
Haoyan Min <hm2689@columbia.edu>
2017-05-02 14:57:16 -0700

    Add files via upload

---
+++ push_server_to_kafka.py
@@ -0,0 +1,10 @@
+import json
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
+
+def push(topic,tuple):
+	global producer
+	producer.send(topic, tuple)
+	print '\n\npushed to', topic
